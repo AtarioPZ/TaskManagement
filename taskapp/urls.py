d7ec924ac2486b81c9b8290dcc11d76b59bd8c26
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2023-06-23 19:49:10 +0530

    login/signup page

--- taskapp/urls.py
+++ taskapp/urls.py
@@ -9,6 +9,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path("", views.home, name='home'),
     path("home/", views.home, name='home'),
+    path("login/", views.login, name="login"),
+    path('signup/', views.signup, name='signup'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
