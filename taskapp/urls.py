ef5a77a0a99e44d65441f3446df1b9f10d9abf71
AtarioPZ <87065323+AtarioPZ@users.noreply.github.com>
2023-06-24 21:25:31 +0530

    homepage and login

--- taskapp/urls.py
+++ taskapp/urls.py
@@ -9,8 +9,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path("", views.home, name='home'),
     path("home/", views.home, name='home'),
-    path("login/", views.login, name="login"),
+    path("login/", views.user_login, name="user_login"),
+    path('logout/', views.user_logout, name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- taskapp/views.py
+++ taskapp/views.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
-def login(request):
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # Authenticate user
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # Login user
+            login(request, user)
+            # Redirect to dashboard page
+            return redirect('dashboard')
+        else:           
+            messages.error(request, 'Invalid username or password.')
+        
     return render(request, 'login.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
 
 def signup(request):
     return render(request, 'signup.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
